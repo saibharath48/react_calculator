7cb71743a83b41a1f680e951bdd2b63bde304071
Sai Bharat Kumar <48505814+saibharath48@users.noreply.github.com>
2022-09-16 10:38:21 +0530

    first commit

---
+++ src/OperationButton.js
@@ -0,0 +1,9 @@
+import { ACTIONS } from "./calcu-demo"
+
+export default function OperationButton({dispatch, operation}){
+    return(
+        <button onClick={()=> dispatch({type : ACTIONS.CHOOSE_OPERATION, payload:{operation}})}>
+            {operation}
+        </button>
+    )
+}   
